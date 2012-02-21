0fc5521367cdd1e7c123233686b46c0d03877947
Chris Marshall <chris@chrismar035.com>
2012-02-21 16:55:42 -0500

    Moduled generators

--- lib/generators/chunk/chunk_generator.rb
+++ lib/generators/chunk/chunk_generator.rb
@@ -1,18 +1,20 @@
 require 'rails/generators/migration'
 
-class ChunkGenerator < Rails::Generators::Base
-  include Rails::Generators::Migration
+module GoogleSafeBrowsing
+  module Generators
+    class ChunkGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
 
-  def self.source_root
-    File.expand_path("../templates", __FILE__)
-  end
+      source_root File.expand_path("../templates", __FILE__)
 
-  def self.next_migration_number(path)
-    Time.now.utc.strftime("%Y%m%d%H%M%S")
-  end
+      def self.next_migration_number(path)
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
 
-  def create_model_file
-    template 'chunk.rb', 'app/models/chunk.rb'
-    migration_template 'create_chunks', "db/migrate/create_chunks"
+      def create_model_file
+        template 'chunk.rb', 'app/models/chunk.rb'
+        migration_template 'create_chunks', "db/migrate/create_chunks"
+      end
+    end
   end
 end
