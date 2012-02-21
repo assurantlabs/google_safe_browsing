0caa854647840fea907aa2d27a13d86766e1f31d
Chris Marshall <chris@chrismar035.com>
2012-02-21 16:23:00 -0500

    Fixed VERSION const

--- lib/google_safe_browsing.rb
+++ lib/google_safe_browsing.rb
@@ -1,12 +1,12 @@
 require 'net/http'
 require 'open-uri'
-require File.dirname(__FILE__) + '/google_safe_browsing/version.rb'
+#require File.dirname(__FILE__) + '/google_safe_browsing/version.rb'
 
 module GoogleSafeBrowsing
 
   CLIENT  = 'api'
   API_KEY = 'ABQIAAAAyLR3IaNHXuIIDgTUlo9YORTqV6MDxWSrNbRxMC53QkjhMk0eYw'
-  APP_VER = GoogleSafeBrowsing::VERSION
+  APP_VER = VERSION
   P_VER   = '2.2'
   HOST    = 'http://safebrowsing.clients.google.com/safebrowsing'
   PARAMS  = { :client => CLIENT, :apikey => API_KEY, :appver => APP_VER, 
