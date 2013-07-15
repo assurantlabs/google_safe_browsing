098b664a43608d9c4ae3c20f21786b1074186724
Pawan Poudel <pawan.poudel@gmail.com>
2013-07-15 12:10:31 -0400

    Ensure lookup_url_hashes returns nil if called with empty hashes

--- spec/api_v2_spec.rb
+++ spec/api_v2_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe GoogleSafeBrowsing::APIv2 do
-  describe 'lookup' do
-    it 'should return nil for invalid urls' do
-      GoogleSafeBrowsing::APIv2.lookup('asdlkfjasd;l').should be_nil
+  describe '#lookup' do
+    it 'returns nil for invalid urls' do
+      expect(GoogleSafeBrowsing::APIv2.lookup('asdlkfjasd;l')).to be_nil
+    end
+  end
+
+  describe '#lookup_url_hashes' do
+    it 'returns nil for empty hashes' do
+      expect(GoogleSafeBrowsing::APIv2.lookup_url_hashes(Array.new)).to be_nil
     end
   end
 end
