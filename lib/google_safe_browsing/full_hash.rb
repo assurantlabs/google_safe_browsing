b9f28fa93dc64d109a7b4bf96872ef59778eefdb
Chris Marshall <chris@chrismar035.com>
2013-11-25 11:33:50 -0500

    FullHashes can delete themeseves when "subbed"
    When a sub chunk is added for an already fetched
    FullHash, that full hash should no longer be
    matched, as it's removed from the database. Thus
    we should delete them from our local cache of full
    hashes.

--- lib/google_safe_browsing/full_hash.rb
+++ lib/google_safe_browsing/full_hash.rb
@@ -1,5 +1,16 @@
 module GoogleSafeBrowsing
   class FullHash < ActiveRecord::Base
     self.table_name = 'gsb_full_hashes'
+
+    def self.delete_subbed
+      sub_join = <<-SQL
+        INNER JOIN gsb_sub_shavars
+        ON gsb_sub_shavars.add_chunk_number = gsb_full_hashes.add_chunk_number
+        AND gsb_sub_shavars.list = gsb_full_hashes.list
+      SQL
+
+      hash_ids = joins(sub_join).pluck("distinct #{self.table_name}.id")
+      where(id: hash_ids).delete_all
+    end
   end
 end
