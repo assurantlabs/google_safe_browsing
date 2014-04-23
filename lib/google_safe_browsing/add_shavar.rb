26e1a155379b8f34ae4901716e46b9635a4987a0
Chris Marshall <chris@chrismar035.com>
2014-04-23 16:53:57 -0400

    Properly extend Shavar delete chunks class method

--- lib/google_safe_browsing/add_shavar.rb
+++ lib/google_safe_browsing/add_shavar.rb
@@ -1,6 +1,7 @@
 module GoogleSafeBrowsing
   class AddShavar < ActiveRecord::Base
     include Shavar
+    extend Shavar::ClassMethods
 
     self.table_name = 'gsb_add_shavars'
   end
--- lib/google_safe_browsing/shavar.rb
+++ lib/google_safe_browsing/shavar.rb
@@ -1,7 +1,9 @@
 module GoogleSafeBrowsing
   module Shavar
-    def self.delete_chunks_from_list(list, chunk_list)
-      self.class.where(list: list, chunk_number: chunk_list.to_a).delete_all
+    module ClassMethods
+      def delete_chunks_from_list(list, chunk_list)
+        where(list: list, chunk_number: chunk_list.to_a).delete_all
+      end
     end
   end
 end
